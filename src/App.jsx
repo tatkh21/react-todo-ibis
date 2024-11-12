a80c7ee85a61a0c4f68c516b75c8b34bf924c712
tatkh21 <tatkh21@gmail.com>
2024-11-11 20:50:02 -0600

    Created unordered list for lesson 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const headertTitle = 'Todo List';
+const todoList = [
+  { id: 1,
+    title: ' Task 1'
+  },
+  { id: 2,
+    title: ' Task 1'
+    
+  },{
+    id: 3,
+    title: ' Task 3'
+    
+  }];
 
+
+function App() {
+ 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>{headertTitle}</h1>
+        <ul>
+        {todoList.map(function (item) {
+          return (
+            <li key={item.objectID}>
+              <span>{item.id}</span>
+              <span>{item.title}</span>
+            </li>
+          );
+        })}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
